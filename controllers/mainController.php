b3092ce0131a263ac609b1354389bb128060a992
YounesHarrat <harratanis@hotmail.fr>
2021-05-03 15:21:12 +0200

    -connecting to database 'film'

--- controllers/mainController.php
+++ controllers/mainController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controllers;
 
+use App\Models\Model;
+
 class MainController {
+
+    
     
     # classe à appeler si on ne renseigne pas de paramètre dans l'url
     public function index() {
