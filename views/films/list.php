f5cfa8602e61ce55805f2efc3922c20bd893c315
YounesHarrat <harratanis@hotmail.fr>
2021-05-03 17:06:11 +0200

    rating stars not working css

--- views/films/list.php
+++ views/films/list.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Liste des Films</title>
+        <link rel="stylesheet" href="ratingStars.css" >
     </head>
     <body>
 
@@ -14,10 +15,14 @@
                 echo "<div style='justify-content: space-around;text-align:center;' > ";
                 echo "<br>";
                 echo "<img src='" . $f->affiche . "' style='width:240px;height:400px;'/>";
+                echo '<div class="rating">
+                <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                </div>';
                 echo '<p> <b>' . $f->nom . '</b>.</p>';
                 echo '<p> Date de Sortie : [ ' . $f->dateSortie . '].</p>';
                 echo '<p> Durée : ' . $f->duree . '.</p>';
                 echo '<p> Acteurs : <b> ' . $f->acteurs . '</b>.</p>';
+
                 // echo '<p> Affiche : ' . $f->affiche . '.</p>';
                 echo "</div>";
             }
