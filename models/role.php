4855227eec72e0c20c386aefa41e4ff486bfe67f
YounesHarrat <harratanis@hotmail.fr>
2021-05-03 15:27:18 +0200

    -model et controller

---
+++ models/role.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\models;
+use App\Core\Db;
+use PDO;
+
+class Model extends Db {
+    private $db;
+
+    public function __construct() {
+
+        # on instancie le singleton
+        $this->db = Db::getInstance();
+    }
+    # méthode permettant de récupérer l'entièreté des users
+    public function findAll() {
+        $find = $this->db->query('SELECT * FROM film.role');
+        $find = $find->fetchAll(PDO::FETCH_OBJ);
+        return $find;
+    }
+    # méthode permettant de récupérer un user en particulier via son $id
+    public function findOne($id) {
+        $find = $this->db->query('SELECT * FROM film.role WHERE id='.$id);
+        $try = $find->fetchAll(PDO::FETCH_OBJ);
+        return $try;
+    }
+}
